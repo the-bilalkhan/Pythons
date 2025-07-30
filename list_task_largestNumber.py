1d571857fc023380b94f8abc0bd748c4098ce290
Muhammad Bilal Khan <bilalazharkhan1@gmail.com>
2025-07-30 22:27:40 +0500

    Delete list_task_largestNumber.py

--- list_task_largestNumber.py
+++
@@ -1,9 +0,0 @@
-#program to find the largest number?
-
-number = [10, 34, 65, 109, 4500, 10000, 23000, 23, 65, 765, 76532, 87, 9]
-
-max = number[0]
-for num in number:
-    if num > max:
-        max = num
-print(max)
